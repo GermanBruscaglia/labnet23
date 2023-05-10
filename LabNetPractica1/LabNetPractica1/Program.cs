8235db2b5b66e04fdd413917bc8a84ba4c5a9ae0
German Bruscaglia <germanbruscaglia@gmail.com>
2023-05-09 23:14:59 -0300

    Se sube Practica1
    Se suben correcciones de  ejercicios resueltos de la practica 1 de poo

--- LabNetPractica1/LabNetPractica1/Program.cs
+++ LabNetPractica1/LabNetPractica1/Program.cs
@@ -10,95 +10,39 @@ namespace LabNetPractica1
     {
         static void Main(string[] args)
         {
-            //List<Omnibus> listaOmnibus = new List<Omnibus>();
+            List<Omnibus> listaOmnibus = new List<Omnibus>();
 
-            //for (var i = 1; i <= 5; i++)
-            //{
-            //    Console.WriteLine($"Cantidad de pasajeros Omnibus{i}");
-            //    listaOmnibus.Add(new Omnibus(Int32.Parse(Console.ReadLine()), $"Omnibus{i}"));
-            //}
+            for (var i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Cantidad de pasajeros Omnibus{i}");
+                listaOmnibus.Add(new Omnibus(Int32.Parse(Console.ReadLine()), $"Omnibus{i}"));
+            }
 
-            string prueba = "12345";
-            int pruebaSalida;
-            int.TryParse(prueba, out pruebaSalida);
+            List<Taxi> listaTaxi = new List<Taxi>();
 
-            Console.WriteLine($"prueba: {prueba} - pruebaSalida: {pruebaSalida}");
-
-            //List<Taxi> listaTaxi = new List<Taxi>();
-
-            //for (var i = 1; i <= 5; i++)
-            //{
-            //    Console.WriteLine($"Cantidad de pasajeros Taxi{i}");
-            //    listaTaxi.Add(new Taxi(Int32.Parse(Console.ReadLine()), $"Taxi{i}"));
-            //}
+            for (var i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Cantidad de pasajeros Taxi{i}");
+                listaTaxi.Add(new Taxi(Int32.Parse(Console.ReadLine()), $"Taxi{i}"));
+            }
 
 
-            //Console.Clear();
-            //Console.WriteLine("Lista de pasajeros de Omnibus");
-            //foreach (var item in listaOmnibus)
-            //{
-            //    Console.WriteLine(item.Listarvehivulos());
-            //}
+            Console.Clear();
+            Console.WriteLine("Lista de pasajeros de Omnibus");
+            foreach (var item in listaOmnibus)
+            {
+                Console.WriteLine(item.Listarvehivulos());
+            }
 
-            //Console.WriteLine("Lista de pasajeros de Taxi");
-            //foreach (var item in listaTaxi)
-            //{
-            //    Console.WriteLine(item.Listarvehivulos());
-            //}
+            Console.WriteLine("Lista de pasajeros de Taxi");
+            foreach (var item in listaTaxi)
+            {
+                Console.WriteLine(item.Listarvehivulos());
+            }
 
             Console.Read();
 
-
-
-
-
-            //        //Coche coche1 = new Coche();
-
-            //        //Coche coche2 = new Coche();
-
-            //        //Console.WriteLine(coche1.GetInfoCoche());
-
-            //        //Coche coche3 = new Coche(4500.5, 1.2);
-            //        //Console.WriteLine(coche3.GetInfoCoche());
-            //        Console.Read();
-            //    }
-            //}
-
-            //class Coche
-            //{
-
-            //    public Coche()
-            //    {
-            //        ruedas = 4;
-            //        largo = 2300.5;
-            //        ancho = 0.800;
-            //    }
-
-            //    public Coche(double anchoCoche, double largoCoche)
-            //    {
-            //        ruedas = 4;
-            //        largo = largoCoche;
-            //        ancho = anchoCoche;
-            //    }
-
-            //    public string GetInfoCoche()
-            //    {
-            //        return $"Informacion del coche: - {ruedas} - {largo} - {ancho}";
-            //    }
-
-            //    private int ruedas;
-
-            //    private double ancho;
-
-            //    private double largo;
-
-            //    private bool climatizador;
-
-            //    private string tapizeria;
-
-
-
         }
-
+        
     }
 }
