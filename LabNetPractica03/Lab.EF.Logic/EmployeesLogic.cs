d8c5397aee638ef8daa55c6cff91c015b4f0d4a3
German Bruscaglia <germanbruscaglia@gmail.com>
2023-05-15 10:46:39 -0300

    Se sube separacion en capas del proyecto
    Creacion de entidades de una base de datos
    Separacion del proyecto en capas
    Ajuste de referencias y utilizacion de Entity Framework

---
+++ LabNetPractica03/Lab.EF.Logic/EmployeesLogic.cs
@@ -0,0 +1,19 @@
+﻿using Lab.EF.Data;
+using Lab.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.Logic
+{
+    public class EmployeesLogic : BaseLogic
+    {
+
+        public List<Employees> GetAll()
+        {
+            return _nortwindContext.Employees.ToList(); 
+        }
+    }
+}
---
+++ LabNetPractica03/Lab.EF.UI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Lab.EF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.EF.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Empleados");
+
+            EmployeesLogic employesLogic = new EmployeesLogic();
+
+            foreach(var emp in employesLogic.GetAll())
+            {
+                Console.WriteLine(emp.FirstName);
+            }
+
+            Console.Read();
+        }
+    }
+}
